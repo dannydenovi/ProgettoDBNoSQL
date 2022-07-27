f85557be7ce936babfb9581f2f3de225cb32fb0a
Claudio Anchesi <claudio.anchesi@outlook.it>
2022-07-27 20:06:38 +0200

    Aggiunta argparse.

--- insert.py
+++ insert.py
@@ -2,9 +2,30 @@ from sys import argv, exit
 from scripts.mongo_insert import insert_mongo
 from scripts.neo_insert import insert_neo
 from threading import Thread
+import argparse
 
 
-debug = mongo = neo = False
+parser = argparse.ArgumentParser(description="Inserts files into either MongoDB or Neo4j databases, or both of them.")
+parser.add_argument('-d', '--debug',
+                    action="store_true",
+                    default=False,
+                    help='Debugging, it deletes all collections in \'test\' database.')
+parser.add_argument('-m', '--mongo',
+                    action="store_true",
+                    default=False,
+                    help='Insert files only to MongoDB database.')
+parser.add_argument('-n', '--neo',
+                    action="store_true",
+                    default=False,
+                    help='Insert files only to Neo4j database.')
+parser.add_argument('-a', '--all',
+                    action="store_true",
+                    default=False,
+                    help='Insert files only to Neo4j database.')
+
+args    = parser.parse_args()
+debug   = args.debug
+mongo, neo = (args.mongo, args.neo) if not args.all else (args.all, args.all)
 
 if len(argv) - 1:
     if "-d" in argv:
--- scripts/mongo_insert.py
+++ scripts/mongo_insert.py
@@ -4,28 +4,30 @@ from csv import DictReader
 
 def insert_mongo(ip: str = "localhost", port: str = "27017", debug: bool = False):
     conn_str = "mongodb://"+ip+":"+port+"/test?retryWrites=true&w=majority"
-    db = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000).test
-
+    client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
+    db = client.test
     if debug:
-        db.people.drop()
-        db.calls.drop()
-        db.cells.drop()
+        client.drop_database('test')
+        #db.people.drop()
+        #db.calls.drop()
+        #db.cells.drop()
 
-    with open("../csv/people.csv") as peoplef:
+    del client
+    with open("csv/people.csv") as peoplef:
         rows = DictReader(peoplef)
         db.people.create_index("Number", unique=True)
         db.people.insert_many(rows)
         if debug:
             print("MONGODB: Persone inserite")
 
-    with open("../csv/cells.csv") as cellsf:
+    with open("csv/cells.csv") as cellsf:
         rows = DictReader(cellsf)
         db.cells.create_index([("City", 1), ("State", 2), ("Address", 3)], unique=True)
         db.cells.insert_many(rows)
         if debug:
             print("MONGODB: Celle inserite")
 
-    with open("../csv/calls.csv") as callsf:
+    with open("csv/calls.csv") as callsf:
         rows = DictReader(callsf)
         db.calls.insert_many(rows)
         if debug:
